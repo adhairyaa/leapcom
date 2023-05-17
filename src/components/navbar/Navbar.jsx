a9342ec088cbb60971040753fba9d49491ccc48f
Dhairya <dhairyagulati370@gmail.com>
2023-05-17 17:27:12 +0530

    add : navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">leapcom</div>
+      <button>cart</button>
+    </div>
+  );
+}
+
+export default Navbar;
