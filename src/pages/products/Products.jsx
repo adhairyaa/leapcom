916cf29ed563f6c2eb5e27e362f177e49b1f9988
Dhairya <dhairyagulati370@gmail.com>
2023-05-18 23:12:53 +0530

    fix : API error

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,12 +1,13 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { getProducts } from "../../services/productServices";
 import "./Products.css";
 import Loader from "../../components/loader/Loader";
+import { updateCart } from "../../services/cartServices";
+import { useCartContext } from "../../contexts/CartProvider";
 function Products() {
   const [products, setProducts] = useState();
   const [isLoading, setIsLoading] = useState(true);
-
+  const { dispatch } = useCartContext();
   useEffect(() => {
     (async () => {
       const response = await getProducts();
@@ -14,7 +15,19 @@ function Products() {
       setIsLoading(false);
     })();
   }, []);
-  console.log(products);
+
+  const handleAddToCart = async (product) => {
+    const res = await updateCart({
+      productId: product._id,
+      quantity: 1,
+    });
+    if (res.data.success) {
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: { product: product, quantity: 1 },
+      });
+    }
+  };
 
   return isLoading ? (
     <Loader />
@@ -23,13 +36,18 @@ function Products() {
       <div className="products-section">
         {products.map((product) => (
           <div className="product-card">
-            <img src={product.img} alt="product-img"></img>
+            <img src={product.img} alt="product-img" loading="lazy"></img>
             <div className="product-card-options">
               <div className="product-card-details">
                 <p>{product.name}</p>
                 <p>{product.price}</p>
               </div>
-              <button className="add-to-cart">Add to Cart</button>
+              <button
+                className="add-to-cart"
+                onClick={() => handleAddToCart(product)}
+              >
+                Add to Cart
+              </button>
             </div>
           </div>
         ))}
