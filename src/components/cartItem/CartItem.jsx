18a1d9eca868b99c08651d0404a1e335277dde04
Dhairya <dhairyagulati370@gmail.com>
2023-05-18 21:01:07 +0530

    update : quantity button logic

--- src/components/cartItem/CartItem.jsx
+++ src/components/cartItem/CartItem.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CartItem.css";
+import { deleteCartItem, updateCart } from "../../services/cartServices";
 function CartItem({ product, quantity }) {
-  const { name, price, img } = product;
-  const totalPrice = price * quantity;
+  const [itemQuantity, setItemQuantity] = useState(quantity);
+  const [isLoading, setIsLoading] = useState(false);
+  const { name, price, img, _id } = product;
+  const totalPrice = price * itemQuantity;
+
+  const handleQuantityUpdate = async (quantity) => {
+    setIsLoading(true);
+    const res = await updateCart({ productId: _id, quantity });
+    if (res.data.success) {
+      setIsLoading(false);
+      setItemQuantity(quantity);
+    }
+  };
+  console.log(isLoading);
   return (
     <div className="cart-items">
       <div className="cart-item-name">
@@ -11,11 +24,26 @@ function CartItem({ product, quantity }) {
       </div>
       <p>{price}</p>
       <div className="cart-item-quantity">
-        <button>+</button>
-        <p>{quantity}</p>
-        <button>-</button>
+        <button
+          className={isLoading ? "disabled" : ""}
+          onClick={() => handleQuantityUpdate(itemQuantity + 1)}
+          disabled={isLoading}
+        >
+          +
+        </button>
+        <p>{itemQuantity}</p>
+        <button
+          className={isLoading || itemQuantity === 1 ? "disabled" : ""}
+          onClick={() => handleQuantityUpdate(itemQuantity - 1)}
+          disabled={isLoading || itemQuantity === 1}
+        >
+          -
+        </button>
       </div>
-      <p>{totalPrice}</p>
+      <div>
+        <p>{totalPrice}</p>
+        <button onClick={() => deleteCartItem(_id)}>X</button>
+      </div>
     </div>
   );
 }
