aef6acf9088575e965e7738e44655fb1f8546c14
Dhairya <dhairyagulati370@gmail.com>
2023-05-18 15:17:08 +0530

    add : loader component

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { getProducts } from "../../services/productServices";
+import "./Products.css";
+import Loader from "../../components/loader/Loader";
 function Products() {
   const [products, setProducts] = useState();
   const [isLoading, setIsLoading] = useState(true);
@@ -15,21 +17,23 @@ function Products() {
   console.log(products);
 
   return isLoading ? (
-    <div>loading....</div>
+    <Loader />
   ) : (
     <div className="products-page">
-      {products.map((product) => (
-        <div className="product-card">
-          <img src={product.img} alt="product-img"></img>
-          <div className="product-card-options">
-            <div className="product-card-details">
-              <p>{product.name}</p>
-              <p>{product.price}</p>
+      <div className="products-section">
+        {products.map((product) => (
+          <div className="product-card">
+            <img src={product.img} alt="product-img"></img>
+            <div className="product-card-options">
+              <div className="product-card-details">
+                <p>{product.name}</p>
+                <p>{product.price}</p>
+              </div>
+              <button className="add-to-cart">Add to Cart</button>
             </div>
-            <button className="add-to-cart">Add to Cart</button>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
