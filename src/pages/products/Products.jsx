0379d761a42a2e488720ebf887540f982021daec
Dhairya <dhairyagulati370@gmail.com>
2023-05-18 13:31:41 +0530

    add : product page

---
+++ src/pages/products/Products.jsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+
+function Products() {
+  useEffect();
+  return <div className="products-page">Products</div>;
+}
+
+export default Products;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/home/home";
+import Home from "../pages/home/Home";
+import Products from "../pages/products/Products";
 
 function Router() {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </div>
